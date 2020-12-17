5358d7bb5a4d545a42f29b16dcb339fb2d82aa8e
Baturay Yasar <mehmetbaturayyasar@gmail.com>
2020-12-17 21:15:28 +0100

    First commit

---
+++ src/com/example/singletonpattern/Main.java
@@ -0,0 +1,9 @@
+package com.example.singletonpattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Database database = Database.getInstance("db_music");
+        System.out.println(database.toString());
+    }
+}
